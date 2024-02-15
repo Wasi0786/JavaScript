4ed39a4a657c5db0195066869e764871971ecf33
Wasi Ahmad <wasibca2020@gmail.com>
2024-02-15 12:30:56 +0530

    conversion Oprations

---
+++ basic_01/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = "33hg"
+// console.log(typeof score);
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33hg" => NaN
+//  true => 1; false => 0
+
+
+let isLoggedIn = "wasi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => flase
+// "wasi" => true
+
+
+let num = 33
+let stringNum = String(num)
+console.log(stringNum);
+console.log(typeof stringNum);
