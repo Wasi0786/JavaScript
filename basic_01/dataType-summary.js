bf5adb69557dab9cf9018e444bd164ca5a8ad457
Wasi Ahmad <wasibca2020@gmail.com>
2024-02-15 13:51:35 +0530

    move in folder

---
+++ basic_01/dataType-summary.js
@@ -0,0 +1,37 @@
+// *************** Primirive ***************
+// 7 types: string, number , boolean, null, undefined, bigint, symbol
+
+
+const scroe = 100;
+const scoreVal = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+
+const bigNumber = 4548468484844854565454n
+
+//Refrence (Non Primitive)
+
+// Array, Object, Functions
+
+const array = ["wasi" , "ali", " saheb"]
+
+const myObj = {
+    name:"wasi",
+    age: 21
+}
+ 
+const myFunction= function() {
+console.log("hello world");
+}
+
+// console.log(typeof bigNumber);
+
+console.log("array..." +  typeof array);
