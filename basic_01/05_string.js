fc711745eb77943e48211731539b4b07191a071c
Wasi Ahmad <wasibca2020@gmail.com>
2024-02-15 21:52:20 +0530

    Strings Methods

---
+++ basic_01/05_string.js
@@ -0,0 +1,61 @@
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value")
+
+// console.log(`name is ${name} the repecount is ${repoCount}`);
+
+const gameName = new String('I am Wasi Ahmad')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.length);
+// console.log(gameName.italics());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("a"));
+
+// console.log(gameName);
+// console.log(gameName.toString());
+
+// console.log(gameName);
+// console.log(gameName.trim());
+// console.log(gameName.trimEnd())
+// console.log(gameName.trimStart())
+// console.log(gameName.valueOf())
+
+
+// console.log(gameName.at(1))
+
+// console.log(gameName.concat(" heloo ", gameName))
+// console.log(gameName.endsWith("a"))
+// console.log(gameName.startsWith("W"))
+
+// console.log(gameName.lastIndexOf("si"))
+
+// console.log("Blue Whale, Killer Whale".lastIndexOf("blue")); // returns -1
+
+
+// const str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+// const regexp = /[A-G]/gi;
+// const matches = str.match(regexp);
+
+// console.log(matches);
+
+
+// const mood = "Happy ";
+// console.log(`I feel ${mood.repeat(3)} `);
+
+
+// console.log(gameName.replace("Wasi", "Ali"));
+// console.log(gameName.replaceAll("Wasi", "Ali"));
+// console.log(gameName.search("Ahmad"));
+
+// console.log(gameName.slice(0, 10));
+
+
+
+
