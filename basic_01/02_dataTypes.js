2601563ac831421261e0813825a63694e988a791
Wasi Ahmad <wasibca2020@gmail.com>
2024-02-15 12:04:53 +0530

    dataTypes

---
+++ basic_01/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js Code as a newer Version
+
+// alert(3+3) we are using nodejs , not brower
+
+
+let userName = "wasi"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+// primitive daya types --->
+// number = ki range = 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof null);
+// typeof null is = Object it is also a interview quetion
