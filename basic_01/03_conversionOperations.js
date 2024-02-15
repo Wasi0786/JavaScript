df922b83b4ae707dcd7cb2090b215cc252596980
Wasi Ahmad <wasibca2020@gmail.com>
2024-02-15 13:08:48 +0530

    operatins done

--- basic_01/03_conversionOperations.js
+++ basic_01/03_conversionOperations.js
@@ -23,5 +23,41 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let num = 33
 let stringNum = String(num)
-console.log(stringNum);
-console.log(typeof stringNum);
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+
+// ******************************** Operations ************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+//basic Arthmatic operations
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello";
+let str2 = " wasi"
+
+// console.log(str1+str2);
+
+// console.log("1" + 2); => 12
+// console.log(1 + "2"); => 12
+// console.log("1" + 2 + 2); => 122
+// console.log(1 + 2 + "2" ) ; => 32
+
+// console.log(+true); => 1
+
+// prifix & postfix
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
