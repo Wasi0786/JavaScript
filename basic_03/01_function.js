864dabe67c081aca6541c6ef0c9e66d06f2756b5
Wasi Ahmad <wasibca2020@gmail.com>
2024-02-16 16:33:45 +0530

    play with function

--- basic_03/01_function.js
+++ basic_03/01_function.js
@@ -17,7 +17,7 @@ function addTwoNumbers(num1,num2){
 //   console.log(num1+num2);
 }
 
-const result = addTwoNumbers(3,4)
+// const result = addTwoNumbers(3,4)
 // console.log("Result... ",result);
 
 function logingUserMessage(username = "sam"){
@@ -27,4 +27,39 @@ function logingUserMessage(username = "sam"){
     return `${username} just Logged In`
 }
 
-console.log(logingUserMessage()); 
+// console.log(logingUserMessage()); 
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200,230,450,15,515,12,2));
+
+
+const user = {
+    username: "hitesh",
+    price: 199
+}
+
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.username} and the price is ${anyObject.price}`);
+}
+
+// handleObject(user)
+// handleObject({
+//     username: "sam",
+//     price: 266
+// }
+// )
+
+
+const myNewArray = [100,200,100,600]
+
+function returnSecondValue(getAarry){
+   return getAarry[1]
+}
+
+// console.log(returnSecondValue(myNewArray) );
+// console.log(returnSecondValue([50,10,40,50]));
+
+
