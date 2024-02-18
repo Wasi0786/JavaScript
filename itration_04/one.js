a7c146442d3841532459665186bf3f691c0b3fa8
Wasi Ahmad <wasibca2020@gmail.com>
2024-02-18 21:23:54 +0530

    print table with the help of for loop

---
+++ itration_04/one.js
@@ -0,0 +1,16 @@
+// ********** For Loop ***********
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    console.log(element);
+    
+}
+
+for (let i = 1; i <= 10; i++) {
+    for (let index = 1; index <= 10; index++) {
+        let result = i*index;
+        console.log(result);
+        
+    }
+}
+  
